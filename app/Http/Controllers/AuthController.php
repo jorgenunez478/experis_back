54cb29a5a12af3c8f82c2c5752e39fadc9db8312
jorgenunez478 <135417174+jorgenunez478@users.noreply.github.com>
2023-08-03 05:29:54 -0500

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    /**
+     * Registrar un usuario nuevo
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors());
+        }
+
+        $user = User::Create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer']);
+    }
+
+    /**
+     * Funcion para el inicio de sesion
+     */
+    public function login (Request $request)
+    {
+        if(!Auth::attempt(["email" => $request->email, "password" => $request->password]))
+        {   
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = User::where('email', $request["email"])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Hi ', $user->name,
+            'access_token' => $token,
+            'type_token' => 'Beare',
+            'user' => $user
+        ]);
+    }
+}
